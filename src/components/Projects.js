fc2b794e267ad0199a9cf6b2df475f437d131652
Sofia Cornu <SofiaCornu@outlook.com>
2024-02-21 21:53:59 +0000

    adding projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -26,7 +26,6 @@ export default function Projects() {
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center"
-                  style=" object-fit: contain;"
                   src={project.image}
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
