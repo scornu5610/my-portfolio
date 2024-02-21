27ec19ca1af5246f4dd6e72df9ab430e0ab08a12
Sofia Cornu <SofiaCornu@outlook.com>
2024-02-21 22:03:19 +0000

    adding projects

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export const projects = [
     subtitle: "Philip Morris International",
     description:
       "Carried out the AI Copilots project for the Consumer Centric Innovation department/ Smoke free products (Philip Morris International, Lausanne office). I defined the testing framework and criteria for the evaluation of AI-powered tools on the market, carried out testing for 120 of these, and recommended a selected set for each of the 5 identified use categories. I also coded a web-based interface (using HTML and CSS) to provide the breakdown of testing results, justification for recommended tools, sample generated outputs and specific prompt engineering guides in an easily accessible format for the team. Languages: HTML, CSS, Javascript. Client: Rehouven Libine.",
-    image: "./aistrat.gif",
+    image: "./aistrat2.gif",
     link: "https://reactbootcamp.com",
   },
   {
@@ -14,7 +14,7 @@ export const projects = [
     subtitle: "Nairobi City County",
     description:
       "Carried out a project for the Nairobi City County as part of a 6 person team, bridging the gap between the Nairobi urban development taskforce and general public via a web app used to display active/ upcoming projects and gather feedback on them. Languages: Svelte, Typescript, Javascript, HTML, CSS. Client: Dr. Linda Nkatha.",
-    image: "./project-2.gif",
+    image: "./nairobi.gif",
     link: "https://reactbootcamp.com",
   },
   {
@@ -22,7 +22,7 @@ export const projects = [
     subtitle: "International School of Lausanne",
     description:
       "Created Subject Select (a desktop application) to facilitate the choice of diploma subjects for students in high school. Designed the recommendation algorithm for this based on a set of parameters including previous grades in subjects, career aspirations and the school's timetabling restrictions. Maintained a customer database with the use of MySQL. Languages: Java, SQL. Client: Michael Humphrey.",
-    image: "./project-3.gif",
+    image: "./subject.gif",
     link: "https://reactbootcamp.com",
   },
   {
@@ -30,7 +30,7 @@ export const projects = [
     subtitle: "Personal curiosity",
     description:
       "Researched and coded my own genetic algorithms in order to better understand them as a heuristic for solving optimization and search problems.",
-    image: "./project-4.gif",
+    image: "./genetic.gif",
     link: "https://reactbootcamp.com",
   },
 ];
