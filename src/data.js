eacc143a42896d5556b11ff20201796d3b92fac8
Sofia Cornu <SofiaCornu@outlook.com>
2024-02-21 20:55:02 +0000

    test change

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     subtitle: "Philip Morris International",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
+    image: "./aistrat.gif",
     link: "https://reactbootcamp.com",
   },
   {
