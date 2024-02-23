7014321d652f38bf6c6faa7cdd2dd396775e6c06
Sofia Cornu <SofiaCornu@outlook.com>
2024-02-23 12:06:48 +0000

    removing svg files

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -9,19 +9,19 @@ export default function Carousel(){
       title: "Baseball",
       description:
         "Baseball is a bat-and-ball sport played between two teams of nine players each, taking turns batting and fielding. The game occurs over the course of several plays, with each play generally beginning when a player on the fielding team, called the pitcher.",
-      icon: require("./Media/example1.svg"),
+      
     },
     {
       title: "Walking",
       description:
         "Walking (also known as ambulation) is one of the main gaits of terrestrial locomotion among legged animals. Walking is typically slower than running and other gaits. ",
-      icon: require("./Media/example2.svg"),
+      
     },
     {
       title: "Weights",
       description:
         "Weightlifting generally refers to activities in which people lift weights, often in the form of dumbbells or barbells. People lift various kinds of weights for a variety of different reasons.",
-      icon: require("./Media/example3.svg"),
+      
     },
   ];
   const updateIndex = (newIndex) => {
@@ -52,7 +52,6 @@ export default function Carousel(){
             updateIndex(activeIndex - 1);
           }}
         >
-          <span class="material-symbols-outlined">arrow_back_ios</span>{" "}
         </button>
         <div className="indicators">
           {items.map((item, index) => {
@@ -63,15 +62,6 @@ export default function Carousel(){
                   updateIndex(index);
                 }}
               >
-                <span
-                  className={`material-symbols-outlined ${
-                    index === activeIndex
-                      ? "indicator-symbol-active"
-                      : "indicator-symbol"
-                  }`}
-                >
-                  radio_button_checked
-                </span>
               </button>
             );
           })}
@@ -82,7 +72,6 @@ export default function Carousel(){
             updateIndex(activeIndex + 1);
           }}
         >
-          <span class="material-symbols-outlined">arrow_forward_ios</span>
         </button>
       </div>
     </div>
