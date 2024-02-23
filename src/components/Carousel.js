cb8b24d22c088d8cbf9f35691d8aae7cfb1a7a14
Sofia Cornu <SofiaCornu@outlook.com>
2024-02-23 17:45:18 +0000

    section fix up

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -26,82 +26,82 @@ export default function Carousel(){
             for more information feel free to click on a project!
           </p>
         </div>
-      </div>
-      <div className="carousel mx-auto text-center">
-        <div
-          className="inner"
-          style={{ transform: `translate(-${activeIndex * 100}%)`}} //carousel px-5 py-10 mx-auto text-center lg:px-40
-        >
-          {projects.map((project) => (
-            <div className="carousel-item" style={{ width: "100%" }}>
-              <a
-                href={project.link}
-                key={project.image}
-                className="sm:w-1/2 w-100 p-4">
-                <div className="flex relative">
-                  <img
-                    alt="gallery"
-                    className="absolute inset-0" // w-full h-full object-cover object-center
-                    src={project.image}
-                  />
-                  <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                    <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                      {project.subtitle}
-                    </h2>
-                    <h1 className="title-font text-lg font-medium text-white mb-3">
-                      {project.title}
-                    </h1>
-                    <p className="leading-relaxed">{project.description}</p>
+        <div className="carousel mx-auto text-center">
+          <div
+            className="inner"
+            style={{ transform: `translate(-${activeIndex * 100}%)`}} //carousel px-5 py-10 mx-auto text-center lg:px-40
+          >
+            {projects.map((project) => (
+              <div className="carousel-item" style={{ width: "100%" }}>
+                <a
+                  href={project.link}
+                  key={project.image}
+                  className="sm:w-1/2 w-100 p-4">
+                  <div className="flex relative">
+                    <img
+                      alt="gallery"
+                      className="absolute inset-0" // w-full h-full object-cover object-center
+                      src={project.image}
+                    />
+                    <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                      <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                        {project.subtitle}
+                      </h2>
+                      <h1 className="title-font text-lg font-medium text-white mb-3">
+                        {project.title}
+                      </h1>
+                      <p className="leading-relaxed">{project.description}</p>
+                    </div>
                   </div>
-                </div>
-              </a>
-            </div>
-          ))}
-        </div>
-
-
-        <div className="carousel-buttons">
-          <button
-            className="button-arrow"
-            onClick={() => {
-              updateIndex(activeIndex - 1);
-            }}
-          >
-            <span class="material-symbols-outlined">◁</span>{" "}
-          </button>
-          <div className="indicators">
-            {items.map((item, index) => {
-              return (
-                <button
-                  className="indicator-buttons"
-                  onClick={() => {
-                    updateIndex(index);
-                  }}
-                >
-                  <span
-                    className={`material-symbols-outlined ${
-                      index === activeIndex
-                        ? "indicator-symbol-active"
-                        : "indicator-symbol"
-                    }`}
-                  >
-                    ◉
-                  </span>
-                </button>
-              );
-            })}
+                </a>
+              </div>
+            ))}
           </div>
-          <button
-            className="button-arrow"
-            onClick={() => {
-              updateIndex(activeIndex + 1);
-            }}
-          >
-            <span class="material-symbols-outlined">▷</span>
-          </button>
+
+
+          <div className="carousel-buttons">
+            <button
+              className="button-arrow"
+              onClick={() => {
+                updateIndex(activeIndex - 1);
+              }}
+            >
+              <span class="material-symbols-outlined">◁</span>{" "}
+            </button>
+            <div className="indicators">
+              {items.map((item, index) => {
+                return (
+                  <button
+                    className="indicator-buttons"
+                    onClick={() => {
+                      updateIndex(index);
+                    }}
+                  >
+                    <span
+                      className={`material-symbols-outlined ${
+                        index === activeIndex
+                          ? "indicator-symbol-active"
+                          : "indicator-symbol"
+                      }`}
+                    >
+                      ◉
+                    </span>
+                  </button>
+                );
+              })}
+            </div>
+            <button
+              className="button-arrow"
+              onClick={() => {
+                updateIndex(activeIndex + 1);
+              }}
+            >
+              <span class="material-symbols-outlined">▷</span>
+            </button>
+          </div>
         </div>
-    
       </div>
     </section>
+    
   );
 };
