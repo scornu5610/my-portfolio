98fa5dbf1349257c0b012a0fce1548832c5930ea
Sofia Cornu <SofiaCornu@outlook.com>
2024-02-23 12:01:53 +0000

    finds them, something's failing though

--- src/components/CarouselItem.js
+++ src/components/CarouselItem.js
@@ -1,6 +1,44 @@
 import React from "react";
 
-export default function(){}
+export default function(){
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            <br className="hidden lg:inline-block" /> Driving positive social change through computer science.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            This site contains a professional portfolio of projects I've carried out, 
+            ranging from AI strategy for world-leading companies 
+            to application and algorithm design to support students in high schools.
+            If you would like to know more about any of these projects, 
+            please feel free to email me via the contact form!
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              Work With Me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              See My Past Work
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./mountains.png"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
 /*
 export default function CarouselItem({ item, width }){
   return (
