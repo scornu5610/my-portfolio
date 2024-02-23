714a9eb991ca7164cfb1a9e2c3d34813f145bf20
Sofia Cornu <SofiaCornu@outlook.com>
2024-02-23 14:04:58 +0000

    attempt at getting image to show

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -39,6 +39,12 @@ export default function Carousel(){
         style={{ transform: `translate(-${activeIndex * 100}%)`
      }}
       >
+        <div className="carousel-item" style={{ width: "100%" }}>
+          <div></div>
+          <img className="carousel-img" src= "./Media/example1.svg"/>
+          <div className="carousel-item-text">"Baseball is a bat-and-ball sport played between two teams of nine players each, taking turns batting and fielding. The game occurs over the course of several plays, with each play generally beginning when a player on the fielding team, called the pitcher."</div>
+        </div>
+
         {items.map((item) => {
           return (<div className="carousel-item" style={{ width: "100%" }}>
           <div></div>
