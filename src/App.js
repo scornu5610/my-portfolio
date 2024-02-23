df8e1abb7fa4ee52569c4112bf5b4c720e01d2fe
Sofia Cornu <SofiaCornu@outlook.com>
2024-02-23 11:13:40 +0000

    slider slick import

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
-import Slider from "/components/Slider";
+//import Slider from "/components/Slider";
 
 export default function App() {
   return (
@@ -13,7 +13,6 @@ export default function App() {
       <Navbar />
       <About />
       <Projects />
-      <Slider/>
       <Skills />
       <Testimonials />
       <Contact />
