28383b221006f7b0d7ee2bd830e3faddf7c7eca7
Sofia Cornu <SofiaCornu@outlook.com>
2024-02-23 16:24:31 +0000

    project mappings

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,11 +3,31 @@ import { projects } from "../data";
 
 export default function Carousel(){
   const [activeIndex, setActiveIndex] = useState(0);
+  const items = [
+    {
+      title: "Baseball",
+      description:
+        "Baseball is a bat-and-ball sport played between two teams of nine players each, taking turns batting and fielding. The game occurs over the course of several plays, with each play generally beginning when a player on the fielding team, called the pitcher.",
+      icon: require("./Media/example1.svg"),
+    },
+    {
+      title: "Walking",
+      description:
+        "Walking (also known as ambulation) is one of the main gaits of terrestrial locomotion among legged animals. Walking is typically slower than running and other gaits. ",
+      icon: require("./Media/example2.svg"),
+    },
+    {
+      title: "Weights",
+      description:
+        "Weightlifting generally refers to activities in which people lift weights, often in the form of dumbbells or barbells. People lift various kinds of weights for a variety of different reasons.",
+      icon: require("./Media/example3.svg"),
+    },
+  ];
   const updateIndex = (newIndex) => {
     if (newIndex < 0) {
       newIndex = 0;
-    } else if (newIndex >= projects.length) {
-      newIndex = projects.length - 1;
+    } else if (newIndex >= items.length) {
+      newIndex = items.length - 1;
     }
 
     setActiveIndex(newIndex);
@@ -57,7 +77,7 @@ export default function Carousel(){
           <span class="material-symbols-outlined">arrow_back_ios</span>{" "}
         </button>
         <div className="indicators">
-          {projects.map((project, index) => {
+          {items.map((item, index) => {
             return (
               <button
                 className="indicator-buttons"
